8aec6fd055ea51895ab9f33a11b0f9ed0c206c27
Austen Collins <austencollins@gmail.com>
2013-11-04 22:12:27 -0700

    changes

--- config/auth_google.js
+++ config/auth_google.js
@@ -6,16 +6,11 @@ var app             =   express();
 
 // Gets Google Authentication - TODO: Put these as ENV Configs
 
-var CLIENT_ID       = 	'536518698309.apps.googleusercontent.com'
-var CLIENT_SECRET   = 	'vEtS_zEU9FyGKRSobUzFBoZt'
-if ('development' == app.get('env')) {
-  var REDIRECT_URL    = 	'http://localhost:3000/auth/google/callback'
-};
-if ('production'  == app.get('env')) {
-  var REDIRECT_URL    = 	'http://keep-a-contactv2.herokuapp.com/auth/google/callback'
-};
-var SCOPE           =   'https://www.google.com/m8/feeds'
-var oauth2Client    = 	new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var CLIENT_ID       	= 	'536518698309.apps.googleusercontent.com'
+var CLIENT_SECRET   	= 	'vEtS_zEU9FyGKRSobUzFBoZt'
+var GOOGLE_REDIRECT_URL =   process.env.GOOGLE_REDIRECT_URL  ||  'http://localhost:3000/auth/google/callback';
+var SCOPE           	=   'https://www.google.com/m8/feeds'
+var oauth2Client    	= 	new OAuth2Client(CLIENT_ID, CLIENT_SECRET, GOOGLE_REDIRECT_URL);
 
 	// generates a url that allows offline access and asks permissions
 	// for Google+ scope.
